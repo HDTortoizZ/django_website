e47eea0d0b81b78c9ab74f71649d5f2692b5fbcd
Callum-Scotland <89554188+HDTortoizZ@users.noreply.github.com>
2022-03-30 00:14:38 +0100

    Update views.py
    Created a basic view for partitions

--- data_website/data_partitions/views.py
+++ data_website/data_partitions/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class CreatePartitionsView(TemplateView):
+    def get(self, request, *args, **kwargs):
+        return render('data_partitions/partition', request)
