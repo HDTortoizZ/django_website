1e89a606d62b0018545ce04f02f1c008dbd894c4
Callum-Scotland <89554188+HDTortoizZ@users.noreply.github.com>
2022-03-30 00:15:31 +0100

    Update views.py
    Added .html to the view's render function.

--- data_website/data_partitions/views.py
+++ data_website/data_partitions/views.py
@@ -4,4 +4,4 @@ from django.views.generic import TemplateView
 
 class CreatePartitionsView(TemplateView):
     def get(self, request, *args, **kwargs):
-        return render('data_partitions/partition', request)
+        return render('data_partitions/partition.html', request)
