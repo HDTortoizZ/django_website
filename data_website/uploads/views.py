5498ec88c85fae311ce67a5cf546879ea1cfd39c
Callum-Scotland <89554188+HDTortoizZ@users.noreply.github.com>
2022-03-31 09:32:09 +0100

    Update views.py
    Created a view and a basic template for the views page.

--- data_website/uploads/views.py
+++ data_website/uploads/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from libraries.handlers.uploads import handle_uploaded_file
+from uploads.forms import UploadForm
 
-# Create your views here.
+
+class UploadView(TemplateView):
+    def get(self, request):
+        form = UploadForm()
+        return render(request, 'uploads/upload.html', {'form': form})
+
+    def post(self, request):
+        form = UploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            file = request.FILES['file']
+            filename = file.name
+            print(file, filename)
+            handle_uploaded_file(file, filename)
+            return render(request, 'uploads/upload.html', {'form': UploadForm()})
+
+        return render(request, 'uploads/upload.html', {'form': form})
